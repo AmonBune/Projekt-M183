8fa3c1ff17560a5886f1f18016860cd368800a3f
Jan-Gasser <jan.gasser@bluewin.ch>
2018-01-12 10:54:22 +0100

    Inital-Commit

---
+++ Mini-Blog-Engine/Mini-Blog-Engine/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace Role_Based_Authorization.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public ActionResult Dashboard()
+        {
+            
+            var current_user = (string)Session["username"];
+            var user_roles = MvcApplication.UserRoles;
+            var current_user_role = (string)user_roles[current_user];
+
+            if (current_user_role == "Administrator")
+            {
+
+                // access granted
+
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }        
+    }
+}
---
+++ Mini-Blog-Engine/Mini-Blog-Engine/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Role_Based_Authorization.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+
+            var current_user = (string)Session["username"];
+            var user_roles = MvcApplication.UserRoles;
+            var current_user_role = (string)user_roles[current_user];
+
+            if (current_user_role == "User")
+            {
+                // access granted
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+            return View();
+        }       
+    }
+}
